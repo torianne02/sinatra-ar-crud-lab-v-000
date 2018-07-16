c2ccd49c2a72186f18e701bf12582fea0f3c3df5
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-16 18:32:37 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,8 +20,10 @@ class ApplicationController < Sinatra::Base
   get '/posts' do
     @posts = Post.all
     erb :index
-
   end
 
-  get '/posts/:id'
+  get '/posts/:id' do
+    @post = Post.find_by_id(params[:id])
+    erb :show
+  end
 end
